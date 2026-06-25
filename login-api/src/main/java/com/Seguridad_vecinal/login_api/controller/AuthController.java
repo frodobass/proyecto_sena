54b87a6d12fb41e2ea071de8e96b08d649003cf0
frodobass <134529503+frodobass@users.noreply.github.com>
2026-06-24 21:30:47 -0500

    Add files via upload
     "Creación API Login y Registro"

--- login-api/src/main/java/com/Seguridad_vecinal/login_api/controller/AuthController.java
+++ login-api/src/main/java/com/Seguridad_vecinal/login_api/controller/AuthController.java
@@ -12,8 +12,33 @@ import com.Seguridad_vecinal.login_api.model.Usuario;
 public class AuthController {
 
     @PostMapping("/registro")
-public String registrarUsuario(@RequestBody Usuario usuario) {
-    // Aquí ya puedes usar los datos recibidos, por ejemplo:
-    return "Usuario " + usuario.getUsername() + " recibido correctamente";
-}
+    public String registrarUsuario(@RequestBody Usuario usuario) {
+        return "Usuario registrado correctamente";
+    }
+
+    @PostMapping("/login")
+    public String iniciarSesion(@RequestBody Usuario usuario) {
+        if (usuario.getPassword() == null || usuario.getUsername() == null) {
+            return "Error en la autenticación";
+        }
+
+        // Datos fijos de prueba para evitar pérdidas por reinicio de servidor
+        String miUsernameValido = "adan";
+        String miEmailValido = "adan@correo.com";
+        String miPasswordValida = "12345";
+
+        // 1. Validamos la contraseña
+        if (usuario.getPassword().equals(miPasswordValida)) {
+            
+            // 2. Extraemos lo introducido en el campo username del JSON
+            String identificadorIntroducido = usuario.getUsername();
+            
+            // 3. Comparamos contra el username fijo O el email fijo
+            if (identificadorIntroducido.equals(miUsernameValido) || identificadorIntroducido.equals(miEmailValido)) {
+                return "Autenticación satisfactoria";
+            }
+        }
+        
+        return "Error en la autenticación";
+    }
 }
--- login-api/src/main/java/com/Seguridad_vecinal/login_api/model/Usuario.java
+++ login-api/src/main/java/com/Seguridad_vecinal/login_api/model/Usuario.java
@@ -1,17 +1,20 @@
 package com.Seguridad_vecinal.login_api.model;
 
 public class Usuario {
+    
     private String username;
     private String password;
+    private String email;
 
     // Constructor vacío (Obligatorio para Spring)
     public Usuario() {
     }
 
     // Constructor con parámetros
-    public Usuario(String username, String password) {
+    public Usuario(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     // Getters y Setters (Obligatorios para que Spring lea los datos)
@@ -29,5 +32,13 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+       return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
